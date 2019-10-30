7c975679fe67e7543d000a177b9afae549c975cf
Isuru Fernando <isuruf@gmail.com>
2019-10-30 00:42:54 -0500

    Print log

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
-
 ./configure --prefix=$PREFIX \
             --with-gmp=$PREFIX \
             --enable-static
-make
-make check
+
+make -j${CPU_COUNT}
+make check || (cat tests/test-suite.log && false)
 make install
